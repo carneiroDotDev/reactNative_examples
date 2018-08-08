0634eb33a505decfc94ac8cefb5e7d7ea08b177e
Carneiro <lcarneir@b-s1-22581.dsp.in.de>
2018-08-08 16:33:55 +0200

    rendered new component

--- albums/index.js
+++ albums/index.js
@@ -9,10 +9,11 @@
 
 import React from 'react';
 import { Text, AppRegistry } from 'react-native'; 
+import Header from './src/components/Header';
 
 const App = () => {
     return (
-        <Text> Some text! </Text>
+        <Header />
 );
 };
 
