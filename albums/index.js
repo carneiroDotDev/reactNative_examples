a7fe03eb30b2757c55094e137ea5f08d26385aa7
Carneiro <lcarneir@b-s1-22581.dsp.in.de>
2018-08-08 16:17:30 +0200

    first component created and redered in the phone screen

--- albums/index.js
+++ albums/index.js
@@ -1,7 +1,19 @@
 /** @format */
+// 
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+// 
+// AppRegistry.registerComponent(appName, () => App);
+// 
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import React from 'react';
+import { Text, AppRegistry } from 'react-native'; 
 
-AppRegistry.registerComponent(appName, () => App);
+const App = () => {
+    return (
+        <Text> Some text! </Text>
+);
+};
+
+AppRegistry.registerComponent('albums', () => App);
