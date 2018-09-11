9457a4e8c0e11938323931171e762f9c661f2ef4
Luiz Carneiro <luiz.carneiro@sport1.de>
2018-09-11 18:26:45 +0200

    added more forms

--- auth/src/Components/common/Input.js
+++ auth/src/Components/common/Input.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import { Text, View, TextInput } from 'react-native';
 
-const Input = ({ label, value, onChangeText }) => {
+const Input = ({ label, value, onChangeText, placeholder }) => {
+   const { inputStyle, labelStyle, containerStyle, secureTextEntry } = styles;
    return(
-       <View>
-           <Text> {label} </Text>
+       <View style={containerStyle}>
+            <Text style={labelStyle}> {label} </Text>
+            {console.warn(label)}
            <TextInput 
+                  secureTextEntry={secureTextEntry}
+                  autoCorrect={false}
+                  placeholder={placeholder}
+                  style={inputStyle}
                   value={value}
                   onChangeText={onChangeText}
-                  style={{ height: 20, width: 100}}
                   />
        </View>
    )
+}
+
+const styles = {
+    inputStyle: {
+        color: '#000',
+       paddingLeft: 5,
+        paddingRight: 5,
+        fontSize: 18,
+        lineHeight: 23,
+        flex: 2,
+    },
+    labelStyle: {
+        fontSize: 18,
+        paddingLeft: 20,
+        flex: 1,
+    },
+    containerStyle: {
+        height: 40,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
 }
 
 export { Input };
